0b659bb02842f4b712bfca8e6aef529220afa97d
Angeschossen <!FlashLight2018>
2021-08-20 07:12:57 +0200

    setting

--- src/main/java/me/angeschossen/upgradeablehoppers/api/enums/Setting.java
+++ src/main/java/me/angeschossen/upgradeablehoppers/api/enums/Setting.java
@@ -2,25 +2,10 @@ package me.angeschossen.upgradeablehoppers.api.enums;
 
 public enum Setting {
 
-    SUCTION_ENABLED(0), FILTER_DELETE(1);
+    SUCTION_ENABLED,
+    FILTER_DELETE,
+    FILTER_BLACKLIST,
+    INSERT_BY_ORDER
 
-    private final int id;
 
-    Setting(int id) {
-        this.id = id;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public static Setting getById(int id) {
-        for (Setting setting : values()) {
-            if (setting.getId() == id) {
-                return setting;
-            }
-        }
-
-        throw new IllegalArgumentException("Enum constant does not exist with this id: " + id);
-    }
 }
